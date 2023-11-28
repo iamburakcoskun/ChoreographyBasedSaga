2d615c1847cd57e6dd65b14dcd8750ad3d3da169
Burak Coskun <burakc34@gmail.com>
2023-11-29 02:11:34 +0300

    OrderApi continue..

---
+++ Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Order.API.Dtos;
+using Order.API.Models;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public OrdersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderCreateDto orderCreate)
+        {
+            var newOrder = new Models.Order
+            {
+                BuyerId = orderCreate.BuyerId,
+                Status = OrderStatus.Suspend,
+                Address = new Address { Line = orderCreate.Address.Line, Province = orderCreate.Address.Province, District = orderCreate.Address.District },
+                CreatedDate = DateTime.Now
+            };
+
+            foreach (var item in orderCreate.OrderItems)
+            {
+                newOrder.Items.Add(new OrderItem
+                {
+                    Price=item.Price,
+                    ProductId=item.ProductId,
+                    Count = item.Count
+                });
+            }
+
+            await _context.AddAsync(newOrder);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,8 +1,32 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+
+// Masstransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(configuration.GetConnectionString("RabbitMQ"));
+    });
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(configuration.GetConnectionString("SqlCon"))
+);
+
+// No Need
+//builder.Services.AddMassTransitHostedService();
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
